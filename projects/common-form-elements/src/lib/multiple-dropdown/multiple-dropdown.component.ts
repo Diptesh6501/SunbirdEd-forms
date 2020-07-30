2aa36afc49fcb985c9d448365e7f0d327e34ccc1
diptesh6501 <diptesh1993@gmail.com>
2020-07-30 21:16:34 +0530

    added multi drop down

--- projects/common-form-elements/src/lib/multiple-dropdown/multiple-dropdown.component.ts
+++ projects/common-form-elements/src/lib/multiple-dropdown/multiple-dropdown.component.ts
@@ -22,20 +22,16 @@ export class MultipleDropdownComponent implements OnInit, OnChanges {
   @Input() default?: any;
   @Input() contextData: any;
   @Input() dataLoadStatusDelegate: Subject<'LOADING' | 'LOADED'>;
-  @ViewChild('checkBoxContainer') checkBoxContainer: ElementRef;
-  showList = false;
-  values = [];
-  singleValue: any;
-  showModal: boolean;
-  showValues: Boolean = false;
-  currentOptions: any;
+  values = new Set<FieldConfigOption<any>>();
+  showModal = false;
+  showValues = false;
+  valuesLabel = '';
   options$?: Observable<FieldConfigOption<any>[]>;
   contextValueChangesSubscription?: Subscription;
   constructor() {
   }
 
   ngOnInit() {
-    this.getDefaultValues();
     if (this.context) {
       this.contextValueChangesSubscription = this.context.valueChanges.pipe(
         tap(() => {
@@ -50,7 +46,7 @@ export class MultipleDropdownComponent implements OnInit, OnChanges {
       this.options = [];
     }
 
-    if (this.isOptionsClosure(this.options)) {
+    if (this.isOptionsClosure(this.options) && !this.options$) {
       this.options$ = (this.options as FieldConfigOptionsBuilder<any>)(
         this.formControlRef,
         this.context,
@@ -63,96 +59,48 @@ export class MultipleDropdownComponent implements OnInit, OnChanges {
   getDefaultValues() {
     if (Array.isArray(this.default) && this.isMultiple) {
       this.default.forEach((ele) => {
-        this.values.push(ele);
+        this.values.add(ele);
       });
     } else if (this.default) {
-      this.values.push(this.default);
+      this.values.add(this.default);
     }
   }
 
   openModal() {
     this.showModal = true;
     this.showValues = false;
-    this.storeMutationState();
   }
 
-  async addSelected(checkBoxContainer: HTMLUListElement, options: FieldConfigOption<any>[],
-    option: FieldConfigOption<any>, index: number) {
-    this.currentOptions = options;
-    const inputs = checkBoxContainer.getElementsByTagName('input');
-    if (this.isMultiple) {
-      if (!inputs[index]['checked']) {
-        inputs[index]['checked'] = true;
-        if (!Boolean(this.values.indexOf(option) > -1)) {
-          this.values.push(option);
-        }
-      } else if (inputs[index]['checked']) {
-        inputs[index]['checked'] = false;
-        const id = this.values.indexOf(option);
-        if (id !== -1) {
-          this.values.splice(id, 1);
-        }
+  async addSelected(option: FieldConfigOption<any>) {
+    if (this.values.has(option)) {
+      this.values.delete(option);
+    } else {
+      if (!this.isMultiple) {
+        this.values.clear();
       }
-    } else if (!this.isMultiple) {
-      this.values = [];
-      options.forEach((ele, i) => {
-        if (ele.value !== option.value) {
-          inputs[i]['checked'] = false;
-        } else {
-          inputs[i]['checked'] = true;
-          this.values.push(option);
-          this.singleValue = option.value;
-        }
-      });
-
+      this.values.add(option);
     }
+    this.valuesLabel = Array.from(this.values).map((v) => v.label).join(', ');
   }
 
   onSubmit() {
     if (this.isMultiple) {
-      this.formControlRef.patchValue(this.values.map(v => v.value));
+      this.formControlRef.patchValue(Array.from(this.values).map((v) => v.value));
     } else {
-      this.formControlRef.patchValue(this.singleValue);
+      this.formControlRef.patchValue((Array.from(this.values)[0] && Array.from(this.values)[0].value) || null);
     }
     this.showModal = !this.showModal;
     this.showValues = true;
     this.formControlRef.markAsDirty();
   }
 
-
-  storeMutationState() {
-    if (this.values && this.values.length) {
-      let inputs;
-      const getInputs = setInterval(() => {
-        inputs = this.checkBoxContainer.nativeElement.getElementsByTagName('input');
-        if (inputs && inputs.length) {
-          console.log('======', inputs);
-          clearInterval(getInputs);
-          if (this.values && this.values.length) {
-            this.values.forEach((v) => {
-              this.currentOptions.forEach((o, index) => {
-                if (Boolean(v.label === o.label)) {
-                  inputs[index]['checked'] = true;
-                }
-              });
-            });
-          }
-        }
-      }, 50);
+  onCancel() {
+    this.values.clear();
+    if (this.isMultiple) {
+      this.formControlRef.patchValue([]);
+    } else {
+      this.formControlRef.patchValue(null);
     }
-  }
-
-  async valueDoesExsists(option) {
-    for (let i = 0; i < this.values.length; i++) {
-      if (this.values[i].value.code === option.value.code) {
-        this.values.push(option);
-      }
-    }
-  }
-
-  cancel() {
-    this.values = [];
-    this.formControlRef.patchValue([]);
     this.formControlRef.markAsDirty();
     this.showModal = false;
     this.showValues = false;
